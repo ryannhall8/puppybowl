70035d51e909695d34d948e804c469d50044b001
Ryann Hall <ryannhall@Ryanns-Air.fios-router.home>
2023-12-19 11:24:18 -0500

    showing puppies

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 let players = [];
-const playerslist = document.querySelector('ul');
-
+const playersList = document.querySelector('ul');
 
 function render(){
     const html = players.map(function(player){
-        console.log(player);
-        return `
+    console.log(player);
+     return `
         <li>
-       <h5> ${ player.name } </h5>
-       ${ player.breed }
-       ${ player.status } 
+        <h5> ${ player.name } </h5>
+        ${ player.breed }
+        <img src='${ player.imageUrl }' />
         </li>
         `;
     }).join('');
-    playerslist.innerHTML = html;
+    playersList.innerHTML = html;
 }
-
 render();
 
 async function fetchPlayers(){
     try{
-        const response = await fetch ('https://fsa-puppy-bowl.herokuapp.com/api/2309-FTB-ET-WEB-AM/players');
-        const json = await response.json();
-        players = json.data;
-        render();
+       const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2309-FTB-ET-WEB-AM/players');
+       const json = await response.json();
+       players = json.data.players;
+       console.log(json.data.players);
+       render();
     }
     catch(error){
         console.log(error);
